b8b8a8caaaf12df0d82e252266089cbd3a343871
Ozan Taşar <tasarozan@gmail.com>
2021-08-11 22:02:53 +0300

    Refactor models for mongoose

--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,65 @@
+const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
 const Post = require('./post')
 
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        autopopulate: true,
+      },
+    ],
+    upvotedPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        autopopulate: true,
+      },
+    ],
+    downvotedPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        autopopulate: true,
+      },
+    ],
+  },
+  { timestamps: true }
+)
 class User {
-  constructor(username) {
-    this.username = username
-    this.posts = []
-    this.upvotedPosts = []
-    this.downvotedPosts = []
-  }
-
   async createPost(image, title) {
-    const post = await new Post(image, title)
+    const post = await Post.create({ image, title })
 
     this.posts.push(post)
 
-    return post
+    await this.save()
   }
 
-  upvotePost(post) {
+  async upvotePost(post) {
     this.upvotedPosts.push(post)
     post.upvotes.push(this)
+
+    await this.save()
+    await post.save()
   }
 
-  downvotePost(post) {
+  async downvotePost(post) {
     this.downvotedPosts.push(post)
     post.downvotes.push(this)
+
+    await this.save()
+    await post.save()
   }
 }
 
-module.exports = User
+userSchema.loadClass(User)
+userSchema.plugin(autopopulate)
+
+module.exports = mongoose.model('User', userSchema)
